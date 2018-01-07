2662516f882837f7ab5fc3ab17cfaf32ba22c1a0
Matt <mattjohnrochford@gmail.com>
2018-01-07 09:12:03 -0800

    a

--- game.js
+++ game.js
@@ -10,10 +10,10 @@ const startImg = document.querySelector('img');
 
 var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 hiScoreText.textContent = w;
-canvas.width = canvas.height = w < 700 ? 300 : 600;
+canvas.width = canvas.height = w < 600 ? 300 : 600;
 startImg.height = startImg.width = canvas.width;
 
-const gridSize = w < 700 ? 15 : 20;
+const gridSize = w < 600 ? 15 : 20;
 
 let score = 0;
 let hiScore = 0;
